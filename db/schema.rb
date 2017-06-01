93f8e66587349250ac6fecd7972cd053d1d649d0
otomakine <wolcmd.exe@gmail.com>
2017-06-01 20:14:10 +0000

    new changes

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,9 @@
 
 ActiveRecord::Schema.define(version: 20170422142559) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "categories", force: :cascade do |t|
     t.string   "name"
     t.string   "description"
